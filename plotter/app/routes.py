b716108059551b8a11d811165ade3f80460217a0
tomczykart <tomczyk.art@gmail.com>
2022-09-21 19:44:12 +0200

    Change linking method for url_for

--- plotter/app/routes.py
+++ plotter/app/routes.py
@@ -1,11 +1,14 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import SearchForm
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     form = SearchForm()
+    if form.validate_on_submit():
+        flash(f'Searching for plot{form.search_query}')
+        return redirect(url_for('index'))
     return render_template('index.html', title = 'HOME', form = form)
 
 @app.route('/about')
@@ -15,3 +18,7 @@ def about():
 @app.route('/how')
 def how():
     return render_template('how.html', title = 'HOW THIS WORKS')
+
+@app.route('/faqs')
+def faqs():
+    return render_template('faqs.html', title = 'FAQs')
