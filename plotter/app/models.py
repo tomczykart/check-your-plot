fabf86ad37f191aa26112445ff91bc59b72f85e0
tomczykart <tomczyk.art@gmail.com>
2022-09-22 22:09:50 +0200

    New table for search queries added along with new migration.

--- plotter/app/models.py
+++ plotter/app/models.py
@@ -4,10 +4,16 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    search_plot = db.Column(db.String(120))
-    time_created = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
-    time_updated = db.Column(db.DateTime(timezone=True), onupdate=db.func.now())
-
+    searched_plots = db.relationship('search_query', backref='author', lazy='dynamic')
 #define function when instance is called
     def __repr__(self):
         return f'<User: {self.email}>'
+
+class search_query(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    plot_id = db.Column(db.String(120))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    time_created = db.Column(db.DateTime(timezone=True), index=True, server_default=db.func.now())
+#define function when instance is called
+    def __repr__(self):
+        return f'<Searched query: {self.plot_id}>'
