05bfb1a122f88881faafe98ef7dc6f627de9a0e7
tomczykart <tomczyk.art@gmail.com>
2022-09-22 22:38:04 +0200

    Register commands for flask shell

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import User, search_query
+
+#register commands for flask shell for convinient tesing
+@app.shell_context_processor
+def make_shell_context():
+    return{'db':db, 'User':User, 'search_query':search_query}
